f0e337ffe4dde3d648a86035a9ad32444ed49758
Cheol Kim <kr.ckim91@gmail.com>
2017-05-20 15:18:29 -0700

    new index.php

--- index.php
+++ index.php
@@ -2,14 +2,13 @@
 /**
  * The main template file.
  *
- * @package RED_Starter_Theme
  */
 
 get_header(); ?>
 
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
-
+		<p>index.php</p>
 		<?php if ( have_posts() ) : ?>
 
 			<?php if ( is_home() && ! is_front_page() ) : ?>
@@ -36,5 +35,4 @@ get_header(); ?>
 		</main><!-- #main -->
 	</div><!-- #primary -->
 
-<?php get_sidebar(); ?>
 <?php get_footer(); ?>
